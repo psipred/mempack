65ef7e75aaa22f62e2fb321a107c85c2664ffed9
Daneil Buchan <clone@myself.com>
2016-11-16 16:14:13 +0000

    passed outdir added

--- src/web_wrapper.py
+++ src/web_wrapper.py
@@ -3,7 +3,7 @@ import re
 from subprocess import call
 
 # Usage:
-# python web_wrapper.py /tmp/21ee4184-ac09-11e6-affd-989096c13ee6/21ee4184-ac09-11e6-affd-989096c13ee6.memsat_svm /opt/Code/mempack/run_mempack.pl /tmp/21ee4184-ac09-11e6-affd-989096c13ee6/21ee4184-ac09-11e6-affd-989096c13ee6.mtx /opt/Code/mempack
+# python web_wrapper.py /tmp/21ee4184-ac09-11e6-affd-989096c13ee6/21ee4184-ac09-11e6-affd-989096c13ee6.memsat_svm /opt/Code/mempack/run_mempack.pl /tmp/21ee4184-ac09-11e6-affd-989096c13ee6/21ee4184-ac09-11e6-affd-989096c13ee6.mtx /tmp/21ee4184-ac09-11e6-affd-989096c13ee6/ /opt/Code/mempack/
 
 topology_string = ''
 with open(sys.argv[1], "r") as f:
@@ -16,8 +16,9 @@ topology_string = topology_string.rstrip()
 topology_string = re.sub("-", ",", topology_string)
 exe = sys.argv[2]
 mtx = sys.argv[3]
-mdir = sys.argv[4]
+out = sys.argv[4]
+mdir = sys.argv[5]
 
-args = [exe, "-mtx", "1", "-t", topology_string, '-w', mdir, mtx]
+args = [exe, "-mtx", "1", "-t", topology_string, '-w', mdir, "-j", out, mtx]
 print(" ".join(args))
-# call(args)
+call(args)
